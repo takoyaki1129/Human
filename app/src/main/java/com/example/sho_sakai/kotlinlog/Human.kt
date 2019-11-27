21ddb6c24d032f2b3e4882aac493da7a4c171ade
Sho_SAKAI <sakaisho_1984@yahoo.co.jp>
2019-11-27 21:01:28 +0900

    Modify issues

--- app/src/main/java/com/example/sho_sakai/kotlinlog/Human.kt
+++ app/src/main/java/com/example/sho_sakai/kotlinlog/Human.kt
@@ -12,10 +12,11 @@ open class Human: Animal, Thinkable {
 
     // Animalクラスのメソッドをオーバーライド
     override fun say() {
-        Log.d("kotlintest", this.name + "(" + this.age + "歳)")
+        Log.d("kotlintest", "私の名前は" + this.name + "です。")
+        Log.d("kotlintest", "年は" + this.age + "歳です。")
     }
 
     override fun think() {
-        Log.d("kotlintest", this.name + "(" + this.hobby + "歳)")
+        Log.d("kotlintest", "私は" + this.hobby + "について考える。")
     }
 }
